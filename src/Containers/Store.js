a16dad2399d55a2922f8391042a9780bab2e9023
github username <github email address>
2018-11-14 17:44:08 +0000

    kickass

--- src/Containers/Store.js
+++ src/Containers/Store.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import CategoriesContainer from './CategoriesContainer'
 import Purchase from '../Components/Purchase'
-
 import API from '../API'
-
-import { Route } from 'react-router-dom'
 
 export default class Store extends React.Component {
 
@@ -30,7 +27,6 @@ export default class Store extends React.Component {
   }
 
   createTransaktion = (event, currentTotal) => {
-    event.preventDefault()
     if(this.state.currentPurchase.length > 0){
       const newPur = {
         total: currentTotal,
@@ -43,27 +39,24 @@ export default class Store extends React.Component {
         };
         API.postSoldItem(soldItem).then(this.handleDeleteAllButton())
         })})
-        
     } else {
+      event.preventDefault()
       console.log("we should add a error message or something")
     }
   }
 
   render() {
-  return (
-    <Route exact path='/'
-    render={() => <div className="store-welcome">
-      <h1>Welcome back, {this.props.currentUser.name}</h1>
-      <Purchase purchase={this.state.currentPurchase} 
+    return(
+    <div className="store-welcome">
+    <h1>Welcome back, {this.props.currentUser.name}</h1>
+
+    <Purchase purchase={this.state.currentPurchase} 
     removefromPurchase={this.removefromPurchase} 
     handleDeleteAllButton={this.handleDeleteAllButton} 
     createTransaktion={this.createTransaktion}/>
 
       <CategoriesContainer categories={this.props.currentUser.categories} addToPurchase={this.addToPurchase} />
     </div>
-      }
-      />
-    )
-  }
+  )}
 
 }
